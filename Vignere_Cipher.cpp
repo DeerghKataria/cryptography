1dd1fe4de2e96fa5c82ebe5b6e7fa3efe236086c
DeerghKataria <deerghkataria@gmail.com>
2023-09-26 22:03:34 +0530

    Vignere Cipher added

--- Vignere_Cipher.cpp
+++ Vignere_Cipher.cpp
@@ -1,52 +1,60 @@
 #include <iostream>
 #include <string>
 using namespace std;
-class Vig {
-   public:
-      string k;
-   Vig(string k) {
-      for (int i = 0; i < k.size(); ++i) {
-         if (k[i] >= 'A' && k[i] <= 'Z')
-            this->k += k[i];
-         else if (k[i] >= 'a' && k[i] <= 'z')
-            this->k += k[i] + 'A' - 'a';
-      }
-   }
-   string encryption(string t) {
-      string output;
-      for (int i = 0, j = 0; i < t.length(); ++i) {
-         char c = t[i];
-         if (c >= 'a' && c <= 'z')
-            c += 'A' - 'a';
-         else if (c < 'A' || c > 'Z')
-            continue;
-         output += (c + k[j] - 2 * 'A') % 26 + 'A'; //added 'A' to bring it in range of ASCII alphabet [ 65-90 | A-Z ]
-         j = (j + 1) % k.length();
-      }
-      return output;
-   }
-   string decryption(string t) {
-      string output;
-      for (int i = 0, j = 0; i < t.length(); ++i) {
-         char c = t[i];
-         if (c >= 'a' && c <= 'z')
-            c += 'A' - 'a';
-         else if (c < 'A' || c > 'Z')
-            continue;
-         output += (c - k[j] + 26) % 26 + 'A';//added 'A' to bring it in range of ASCII alphabet [ 65-90 | A-Z ]
-         j = (j + 1) % k.length();
-      }
-      return output;
-   }
+
+class VigenereCipher {
+public:
+    string key;
+
+    VigenereCipher(string key) {
+        for (int i = 0; i < key.size(); ++i) {
+            if (key[i] >= 'A' && key[i] <= 'Z')
+                this->key += key[i];
+            else if (key[i] >= 'a' && key[i] <= 'z')
+                this->key += key[i] + 'A' - 'a';
+        }
+    }
+
+    string encrypt(string plaintext) {
+        string ciphertext;
+        for (int i = 0, j = 0; i < plaintext.length(); ++i) {
+            char c = plaintext[i];
+            if (c >= 'a' && c <= 'z')
+                c += 'A' - 'a';
+            else if (c < 'A' || c > 'Z')
+                continue;
+            ciphertext += (c + key[j] - 2 * 'A') % 26 + 'A';
+            j = (j + 1) % key.length();
+        }
+        return ciphertext;
+    }
+
+    string decrypt(string ciphertext) {
+        string plaintext;
+        for (int i = 0, j = 0; i < ciphertext.length(); ++i) {
+            char c = ciphertext[i];
+            if (c >= 'a' && c <= 'z')
+                c += 'A' - 'a';
+            else if (c < 'A' || c > 'Z')
+                continue;
+            plaintext += (c - key[j] + 26) % 26 + 'A';
+            j = (j + 1) % key.length();
+        }
+        return plaintext;
+    }
 };
+
 int main() {
-   Vig v("WELCOME");
-   string ori ="deerghkataria";
-   string encrypt = v.encryption(ori);
-   string decrypt = v.decryption(encrypt);
-   cout<<"Deergh Kataria A2305220099"<<endl;
-   cout<<" "<<endl;
-   cout << "Original Message: "<<ori<< endl;
-   cout << "Encrypted Message: " << encrypt << endl;
-   cout << "Decrypted Message: " << decrypt << endl;
+    VigenereCipher vigenere("WELCOME");
+    string originalMessage = "deerghkataria";
+    string encryptedMessage = vigenere.encrypt(originalMessage);
+    string decryptedMessage = vigenere.decrypt(encryptedMessage);
+
+    cout << "Deergh Kataria A2305220099" << endl;
+    cout << " " << endl;
+    cout << "Original Message: " << originalMessage << endl;
+    cout << "Encrypted Message: " << encryptedMessage << endl;
+    cout << "Decrypted Message: " << decryptedMessage << endl;
+
+    return 0;
 }
